50976592a83708f7d52bd0460732dc95a65b1ef7
Richard Harrah <topplethenunnery@gmail.com>
2021-11-28 12:59:13 -0500

    fix extra import

--- src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsJavaPlugin.kt
+++ src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsJavaPlugin.kt
@@ -1,6 +1,5 @@
 package dev.mythicdrops.gradle.conventions
 
-import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.compile.JavaCompile
