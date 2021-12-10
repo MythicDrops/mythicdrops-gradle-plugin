909053cdd782fad4c4a433c49987f9bd46313a83
Richard Harrah <topplethenunnery@gmail.com>
2021-12-10 15:09:33 -0500

    fix detekt findings

--- src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsJavaExtension.kt
+++ src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsJavaExtension.kt
@@ -1,10 +1,12 @@
 package dev.mythicdrops.gradle.conventions
 
+import org.gradle.api.JavaVersion
+
 /**
  * Configuration for selecting which version of Java to compile for.
  *
  * @property javaVersion Version of Java to compile for (e.g., Java 16/17)
  */
 open class MythicDropsJavaExtension {
-    var javaVersion: Int = 16
+    var javaVersion: String = JavaVersion.VERSION_16.majorVersion
 }
