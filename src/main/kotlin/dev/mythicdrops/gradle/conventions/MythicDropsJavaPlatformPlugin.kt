dde71124f749d3d89bb25a17fc0b71f4055da746
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2022-04-20 23:19:05 -0400

    add java-platform plugin (#44)

---
+++ src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsJavaPlatformPlugin.kt
@@ -0,0 +1,20 @@
+package dev.mythicdrops.gradle.conventions
+
+import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+
+/**
+ * Plugin that configures Maven publications for Java Platforms.
+ */
+open class MythicDropsJavaPlatformPlugin : DependentPlugin("Java", "java-platform") {
+    override fun configureProject(target: Project) {
+        target.pluginManager.withPlugin("maven-publish") {
+            target.extensions.getByType<PublishingExtension>().publications.withType<MavenPublication> {
+                from(target.components.getByName("javaPlatform"))
+            }
+        }
+    }
+}
