d7770e2a7b4b26f0fddc3e589426a5d3a670cd05
Richard Harrah <topplethenunnery@gmail.com>
2023-02-19 14:31:58 -0500

    update to gradle 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
     `maven-publish`
@@ -24,63 +21,68 @@ java {
     withSourcesJar()
 }
 
+@Suppress("UnstableApiUsage")
 gradlePlugin {
+    website.set("https://github.com/MythicDrops/mythicdrops-gradle-plugin")
+    vcsUrl.set("https://github.com/MythicDrops/mythicdrops-gradle-plugin")
     plugins {
         create("mythicDropsProject") {
             id = "dev.mythicdrops.gradle.project"
             displayName = "mythicDropsGradleProject"
             description = "Orchestration plugin for all MythicDrops Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.MythicDropsPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsProjectRoot") {
             id = "dev.mythicdrops.gradle.project.root"
             displayName = "mythicDropsGradleProjectRoot"
             description = "Common conventions for MythicDrops root Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.projects.MythicDropsRootPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsProjectBase") {
             id = "dev.mythicdrops.gradle.project.base"
             displayName = "mythicDropsGradleProjectBase"
             description = "Common conventions for all MythicDrops Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.projects.MythicDropsBasePlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsReleaseGitHub") {
             id = "dev.mythicdrops.gradle.release.github"
             displayName = "mythicDropsGradleReleaseGitHub"
             description = "Create a GitHub Release via GitHub Actions for all MythicDrops Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.release.MythicDropsReleaseGitHubPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsConventionJava") {
             id = "dev.mythicdrops.gradle.convention.java"
             displayName = "mythicDropsGradleConventionJava"
             description = "Common conventions for all MythicDrops Java Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.conventions.MythicDropsJavaPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsConventionJavaPlatform") {
             id = "dev.mythicdrops.gradle.convention.java-platform"
             displayName = "mythicDropsGradleConventionJavaPlatform"
             description = "Common conventions for all MythicDrops Java Platform Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.conventions.MythicDropsJavaPlatformPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsConventionKotlinJvm") {
             id = "dev.mythicdrops.gradle.convention.kotlin.jvm"
             displayName = "mythicDropsGradleConventionKotlinJvm"
             description = "Common conventions for all MythicDrops Kotlin JVM Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.conventions.MythicDropsKotlinJvmPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
         create("mythicDropsConventionsMavenPublish") {
             id = "dev.mythicdrops.gradle.convention.maven-publish"
             displayName = "mythicDropsGradle"
             description = "Common conventions for all MythicDrops Maven Publishing Gradle projects."
             implementationClass = "dev.mythicdrops.gradle.conventions.MythicDropsMavenPublishPlugin"
+            tags.set(listOf("kotlin", "pixeloutlaw", "convention"))
         }
     }
-}
-
-pluginBundle {
-    website = "https://github.com/MythicDrops/mythicdrops-gradle-plugin"
-    vcsUrl = "https://github.com/MythicDrops/mythicdrops-gradle-plugin"
-    tags = listOf("kotlin", "pixeloutlaw", "convention")
 }
 
 tasks {
@@ -89,11 +91,6 @@ tasks {
     getByName<Jar>("javadocJar") {
         dependsOn(dokkaJavadoc)
         from(dokkaJavadoc)
-    }
-
-    // compile targeting JDK8
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
     }
 
     // use JUnit Jupiter
