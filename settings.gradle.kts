5522bdc44506b27d852dd1a45c7ed1ae39b94ac6
Richard Harrah <topplethenunnery@gmail.com>
2023-02-19 14:52:41 -0500

    add gradle enterprise tos agreement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.51.0"
+    id("com.gradle.enterprise") version "3.12.2"
 }
 
-rootProject.name = "mythicdrops-gradle-plugin"
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
