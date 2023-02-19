f394f3296914fa533137ea4ff269de6b3b6c5152
Richard Harrah <topplethenunnery@gmail.com>
2023-02-19 13:57:23 -0500

    prepare for kotlin 1.8 usage
    bump java version to 17
    bump ktlint version to 0.48.1
    switch to ktlint-gradle over kotlinter again

--- src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsKotlinJvmPlugin.kt
+++ src/main/kotlin/dev/mythicdrops/gradle/conventions/MythicDropsKotlinJvmPlugin.kt
@@ -12,24 +12,24 @@ import org.jetbrains.dokka.gradle.DokkaPlugin
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.konan.file.File
-import org.jmailen.gradle.kotlinter.KotlinterPlugin
-import org.jmailen.gradle.kotlinter.tasks.FormatTask
-import org.jmailen.gradle.kotlinter.tasks.LintTask
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.KtlintPlugin
 
 /**
- * Plugin that configures Kotlin for JDK 16, enables Detekt, and enables/configures KTLint to use 0.46.0.
+ * Plugin that configures Kotlin for JDK 17, enables Detekt, and enables/configures KTLint to use 0.48.1.
  */
 open class MythicDropsKotlinJvmPlugin : DependentPlugin("Kotlin JVM", "org.jetbrains.kotlin.jvm") {
     override fun configureProject(target: Project) {
-        val javaExtension = target.extensions.getByType<MythicDropsJavaExtension>()
+        val javaExtension =
+            target.extensions.getByType<MythicDropsJavaExtension>().apply {
+                javaVersion.convention(DEFAULT_JAVA_VERSION)
+            }
 
         // apply plugins
         target.pluginManager.apply(DetektPlugin::class.java)
         target.pluginManager.apply(DokkaPlugin::class.java)
-        target.pluginManager.apply(KotlinterPlugin::class.java)
+        target.pluginManager.apply(KtlintPlugin::class.java)
 
-        // configure kotlin to use JDK 16
         target.configure<KotlinJvmProjectExtension> {
             jvmToolchain {
                 languageVersion.set(JavaLanguageVersion.of(javaExtension.javaVersion.get().majorVersion))
@@ -37,11 +37,12 @@ open class MythicDropsKotlinJvmPlugin : DependentPlugin("Kotlin JVM", "org.jetbr
         }
 
         // exclude files from the build directory from being linted or formatted
-        target.tasks.withType<FormatTask> {
-            exclude { fte -> fte.file.absolutePath.contains("${File.separator}build${File.separator}") }
-        }
-        target.tasks.withType<LintTask> {
-            exclude { fte -> fte.file.absolutePath.contains("${File.separator}build${File.separator}") }
+        target.configure<KtlintExtension> {
+            // ktlint version that supports kotlin 1.8.0
+            version.set("0.48.1")
+            filter {
+                exclude("**/build/**")
+            }
         }
 
         target.tasks.withType<KotlinCompile> {
