fbb95fc84c85eaa319c8fe86c04d101d9f5fab1e
Richard Harrah <1672786+ToppleTheNun@users.noreply.github.com>
2024-01-30 20:33:17 -0500

    update to gradle 8.5 (#98)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.60.3"
-    id("com.gradle.enterprise") version "3.15"
+    id("com.gradle.enterprise") version "3.16.2"
 }
 
 gradleEnterprise {
